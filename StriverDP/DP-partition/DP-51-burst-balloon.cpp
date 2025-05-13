2a06baf20fda082866616cdbe7c943671a71347c
Pawan Vishwakarma <pawanvishwakarma31577@gmail.com>
2025-05-13 16:27:15 +0530

    added dp51 code

---
+++ StriverDP/DP-partition/DP-51-burst-balloon.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    public:
+        // refer- https://leetcode.com/problems/burst-balloons/
+        int f(int i, int j, vector<int> &nums, vector<vector<int>> &dp){
+            if(i>j) return 0;
+            if(dp[i][j] != -1)return dp[i][j];
+    
+            int maxi= INT_MIN;
+            for(int ind= i; ind<= j; ind++){
+                int price= nums[i-1]*nums[ind]*nums[j+1] +
+                            f(i, ind-1, nums, dp) +f(ind+1, j, nums, dp);
+                maxi= max(maxi, price);
+            }
+            return dp[i][j]= maxi;
+        }
+        int maxCoins(vector<int>& nums) {
+            int n= nums.size();
+            nums.push_back(1);
+            nums.insert(nums.begin(), 1);
+                // vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+                // return f(1, n, nums, dp);
+                vector<vector<int>>dp(n+2, vector<int>(n+2, 0));
+    
+                for(int i=n; i>=1; i-- ){
+                    for(int j= i; j<=n; j++ ){
+                        int maxi= INT_MIN;
+                        for(int ind= i; ind<= j; ind++){
+                            int price= nums[i-1]*nums[ind]*nums[j+1] +
+                                        dp[i][ind-1] + dp[ind+1][j];
+                            maxi= max(maxi, price);
+                        }
+                        dp[i][j]= maxi;
+                    }
+                }
+    
+                return dp[1][n];
+        }
+    };
