b4b4982397045e3940987d7d751f2c3d130b23d8
Pawan Vishwakarma <pawanvishwakarma31577@gmail.com>
2025-05-15 17:52:17 +0530

    added final code

---
+++ StriverDP/DP-on-rectangles/DP-56-maxi-rect-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    public:
+    //refer- https://leetcode.com/problems/count-square-submatrices-with-all-ones/
+        int countSquares(vector<vector<int>> matrix) {
+            int n= matrix.size();
+            int m= matrix[0].size();
+            vector<vector<int>> dp(n, vector<int>(m,0));
+            // dp= matrix;
+            for(int i=0; i<n; i++) dp[i][0]= matrix[i][0];
+            for(int j=0; j<m; j++) dp[0][j]= matrix[0][j];
+    
+            int ans=0;
+            for(int i=1; i<n; i++){
+                for(int j=1; j<m; j++){
+                    if(matrix[i][j] == 0) dp[i][j]= 0;
+                    else
+                        dp[i][j] = min(dp[i-1][j], min(dp[i-1][j-1], dp[i][j-1]))+1;
+                }
+            }
+    
+            for(int i=0; i<n; i++){
+                for(int j=0; j<m; j++){
+                    ans += dp[i][j];   
+                }
+            }
+            return ans;
+        }
+    };
